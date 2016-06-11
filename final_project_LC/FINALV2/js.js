00b56d89f72488553ba6ab1f992e177ab72f3a5d
lconde7 <laura@happyfamilybrands.com>
2016-06-11 13:02:33 -0400

    updated again!

--- final_project_LC/FINALV2/js.js
+++ final_project_LC/FINALV2/js.js
@@ -14,91 +14,102 @@ $(document).ready(function() {
 
   })
 
-if($(window).width() > 480){
-   $('.icon1').click(function() {
-   $('.icon3').style("margin-left") = "10px";
-})
- };
+// if($(window).width() > 480){
+//    $('.icon1').click(function() {
+//    $('.icon3').style("margin-top") = "10px";
+// })
+//  };
 
 $(".x1").click(function() {
-      $(this).parent().hide();
+     // $(this).parent().hide();
       $('.triangle-up').addClass('hidden');
+      $('.formulas').children().addClass('hidden');
       event.preventDefault();
 }
   );
 
 $(".x2").click(function() {
-      $(this).parent().hide();
+      // $(this).parent().hide();
       $('.triangle-up2').addClass('hidden');
+      $('.formulas').children().addClass('hidden');
+
       event.preventDefault();
 }
   );
 
 $(".x3").click(function() {
-      $(this).parent().hide();
+      // $(this).parent().hide();
       $('.triangle-up3').addClass('hidden');
+      $('.formulas').children().addClass('hidden');
+
       event.preventDefault();
 }
   );
 
 $(".x4").click(function() {
-      $(this).parent().hide();
+      // $(this).parent().hide();
       $('.triangle-up4').addClass('hidden');
+      $('.formulas').children().addClass('hidden');
       event.preventDefault();
 }
   );
 
-    $(".cls-3").click(function() {
-    // First check to see if our current selection is active
-    if ( $(this).hasClass('active') ){
-      // If it is add our hidden Class
-      $('.comparison_ing3').addClass('hidden');
-      // Then remove the active class
-      $(this).removeClass('active');
-    }
-    else{
-      // otherwise add the active class
-      $(this).addClass('active')
-      // Hide any open siblings
-      $('.comparison_ing3').siblings().addClass('hidden');
-      // then show our formula
+
+$(".cls-3").click(function() {
+      $(this).hasClass('active');
+      $('.compare').addClass('hidden');
       $('.comparison_ing3').removeClass('hidden');
-}
-  });
+});
 
 $(".cls-4").click(function() {
-    if ( $(this).hasClass('active') ){
-      $('.comparison_ing2').addClass('hidden');
-      $(this).removeClass('active');
-    }
-    else{
-      // otherwise add the active class
-      $(this).addClass('active')
-      // Hide any open siblings
-      $('.comparison_ing2').siblings().addClass('hidden');
-      // then show our formula
+      $(this).hasClass('active');
       $('.comparison_ing2').removeClass('hidden');
-}
-  });
+});
+
 
 $(".cls-5").click(function() {
-    // First check to see if our current selection is active
-    if ( $(this).hasClass('active') ){
-      // If it is add our hidden Class
-      $('.comparison_ing1').addClass('hidden');
-      // Then remove the active class
-      $(this).removeClass('active');
-      
-      
-    }
-    else{
-      // otherwise add the active class
-      $(this).addClass('active')
-      // Hide any open siblings
-      $('.comparison_ing1').siblings().addClass('hidden');
-      // then show our formula
+      $(this).hasClass('active');
       $('.comparison_ing1').removeClass('hidden');
-}
-  });
+});
+
+//     $(".cls-3").click(function() {
+//     if ( $(this).hasClass('active') ){
+//       $('.comparison_ing3').addClass('hidden');
+//       $(this).removeClass('active');
+//     }
+//     else{
+//       $(this).addClass('active')
+//       $('.compare').addClass('hidden');
+//       $('.comparison_ing3').removeClass('hidden');
+// }
+//   });
+
+// $(".cls-4").click(function() {
+//     if ( $(this).hasClass('active') ){
+//       $('.comparison_ing2').addClass('hidden');
+//       $(this).removeClass('active');
+//     }
+//     else{
+//       $(this).addClass('active')
+//       $('.compare').addClass('hidden');
+//       $('.comparison_ing2').removeClass('hidden');
+// }
+//   });
+
+// $(".cls-5").click(function() {
+//     if ( $(this).hasClass('active') ){
+//       $('.comparison_ing1').addClass('hidden');
+//       $(this).removeClass('active');
+      
+//     }
+//     else{
+//       // otherwise add the active class
+//       $(this).addClass('active')
+//       // Hide any open siblings
+//       $('.compare').addClass('hidden');
+//       // then show our formula
+//       $('.comparison_ing1').removeClass('hidden');
+// }
+//   });
 
 });
